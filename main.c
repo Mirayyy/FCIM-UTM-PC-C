79a1656627ea10330227e909e151adf8e2dab70d
Miray <51432954+Mirayyy@users.noreply.github.com>
2022-09-02 20:51:26 +0300

    Seminar 1 - Probleme spre rezolvare
    1. Se consideră două numere întregi distincte. Să se scrie un
    program care determină numărul mai mare.
    
    2. Se consideră un cerc de raza R şi un pătrat cu latura a.
    Sa se scrie un program care determină dacă cercul încape în pătrat.
    
    3. Se consideră un număr natural citit de la tastatură.
    Să se scrie un program care afişează la ecran dacă numărul este par sau impar.
    
    4. Se consideră trei numere reale.
    Să se scrie un program care determină dacă aceste numere reprezintă lungimile laturilor unui triunghi si, în caz afirmativ, să se:
        • Calculeze perimetrul triunghiului;
        • Determine natura triunghiului în dependentă de lungimile laturilor(isoscel, echilateral, scalen).

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+
+    /*1. Se consideră două numere întregi distincte. Să se scrie un
+    program care determină numărul mai mare.*/
+
+    /*int a, b;
+    printf("Introduceti 2 numere intregi distincte: ");
+    scanf("%i%i", &a, &b);
+    if(a != b && a > b) printf("\nNumarul a este mai mare: %i", a);
+    else if(a != b && a < b) printf("\nNumarul b este mai mare: %i", b);
+    else printf("\nNumarele sunt egale!");
+    return 0;*/
+
+    /*2. Se consideră un cerc de raza R şi un pătrat cu latura a.
+    Sa se scrie un program care determină dacă cercul încape în pătrat.*/
+
+    /*int R, a;
+
+    printf("Introduceti raza R si latura a: ");
+    scanf("%i%i", &R, &a);
+
+    if(a / R >= 2) printf("\nCercul se incadreaza in limitele patratului");
+    else printf("\nCercul nu se incadreaza!");*/
+
+    /*3. Se consideră un număr natural citit de la tastatură.
+    Să se scrie un program care afişează la ecran dacă numărul este par sau impar.*/
+
+    /*int x;
+
+    printf("Introduceti un numar natural: ");
+    scanf("%i", &x);
+
+    if(x / 2 == 0) printf("\nNumarul este par");
+    else printf("\nNumarul este impar!");*/
+
+    /*4. Se consideră trei numere reale.
+    Să se scrie un program care determină dacă aceste numere reprezintă lungimile laturilor unui triunghi si, în caz afirmativ, să se:
+        • Calculeze perimetrul triunghiului;
+        • Determine natura triunghiului în dependentă de lungimile laturilor(isoscel, echilateral, scalen).*/
+
+    /*int a, b, c;
+
+    printf("Introduceti 3 numere: ");
+    scanf("%i%i%i", &a, &b, &c);
+
+    if ((a + b > c) && (a + c > b) && (b + c > a)) {
+        printf("\nAceste numere reprezinta laturile unui triunghi ");
+        if (a == b == c) printf("echilateral. ");
+        else if((a == b) || (b == c) || (a == c)) printf("isoscel. ");
+        else printf("scalen. ");
+    }
+    else printf("\nAceste numere nu reprezinta laturile unui triunghi ");*/
+
+}
